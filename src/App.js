e62092b68992492bde8f1cf330ca189dbdeccf71
Felipe Milioni <felipemilioni@hotmail.com>
2019-08-21 13:57:47 -0300

    routes and node-sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+import Home from "./Home";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+import NotFound from "./NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact activeClassName="active" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/page1">
+              Page 1
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/page2">
+              Page 2
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/page1" component={Page1} />
+        <Route path="/page2" component={Page2} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
